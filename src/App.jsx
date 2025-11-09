c8827c08af7c88ea6e3fd5de46c5b3f853bf4b0d
trongnam13823 <trantrongnam138@gmail.com>
2025-11-09 16:43:41 +0700

    chore: format App.jsx with consistent indentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import router from "./router";
 import { RouterProvider } from "react-router/dom";
 
 function App() {
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
